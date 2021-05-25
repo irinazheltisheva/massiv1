484f8642d0fa4ca5c9b13f76e4d5323c56a5f245
Irina <70594426+irinazheltisheva@users.noreply.github.com>
2021-05-25 09:01:14 +0300

    Initial commit

---
+++ src/ru/geekbrains/lesson3/Main.java
@@ -0,0 +1,40 @@
+package ru.geekbrains.lesson3;
+
+public class Main {
+
+    private static Object String;
+
+    public static void main(String[] args) {
+
+        (); {
+
+            int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+            System.out.println(String[ 0 ]arr.length);
+
+            System.out.println(String[ 1 ]arr.length);
+
+            System.out.println(String[ 2 ]arr.length);
+
+            System.out.println(String[ 3 ]arr.length);
+
+            System.out.println(String[ 4 ]arr.length);
+
+            System.out.println(String[ 5 ]arr.length);
+
+            System.out.println(String[ 6 ]arr.length);
+
+            System.out.println(String[ 7 ]arr.length);
+
+            System.out.println(String[ 8 ]arr.length);
+
+            System.out.println(String[ 9 ]arr.length);
+
+        }
+}
+
+
+
+
+
+
+
